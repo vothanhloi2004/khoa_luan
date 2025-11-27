f30134d54cd277e8d0010322ad9423746c23f419
thanhloi <vothanhloi2004@gmail.com>
2025-11-27 22:14:50 +0700

    Xong frontend , đã chỉnh sửa giao diện để phù hợp với các thiết bị

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 import { Link } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext";
+import nav_dropdown from "../Assets/nav_dropdown.png";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
+  const { getTotalCartItems } = useContext(ShopContext);
+  const menuRef = useRef();
+
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle("nav-menu-visible");
+    e.target.classList.toggle("open");
+  };
 
   return (
     <div className="navbar">
@@ -13,8 +22,14 @@ const Navbar = () => {
         <img src={logo} alt="" />
         <p>Double B</p>
       </div>
+      <img
+        className="nav-dropdown"
+        onClick={dropdown_toggle}
+        src={nav_dropdown}
+        alt=""
+      />
       {/* prettier-ignore */}
-      <ul className="nav-menu">
+      <ul ref={menuRef} className="nav-menu">
         <li onClick={()=> {setMenu("shop")}}> <Link style={{textDecoration:'none'}} to='/' >Shop</Link> {menu==="shop"?<hr/>:<></>} </li>
         <li onClick={()=> {setMenu("mens")}}> <Link style={{textDecoration:'none'}} to='/mens' >Men </Link> {menu==="mens"?<hr/>:<></>} </li>
         <li onClick={()=> {setMenu("womens")}}> <Link style={{textDecoration:'none'}} to='/womens' >Women</Link> {menu==="womens"?<hr/>:<></>} </li>
@@ -25,7 +40,7 @@ const Navbar = () => {
         <Link to='/login'> <button>Login</button> </Link>
         <Link to='/cart'> <img src={cart_icon} alt=""/> </Link>
         
-        <div className="nav-cart-count">0</div>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
     </div>
   );
